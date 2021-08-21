1169a86bdaa8f43b776b0df2f30e73ac93261d03
Ray Colt <57169235+RayColt@users.noreply.github.com>
2021-08-21 09:25:54 +0200

    Delete mc.cpp

--- cpp/mc.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-/*
-* Midnight Commander for Windows
-* Nice C++ executable to put in System32, to run mc system wide like in Linux!!
-*/
-int main()
-{ 
-    system("\"\"C:\\Program Files (x86)\\\Midnight Commander\\\mc.exe\"");
-}
